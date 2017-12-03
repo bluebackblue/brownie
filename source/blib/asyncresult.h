bc82e400c074540c2be7bd160ce1aa91cab4d759
blueback <blueback28@gmail.com>
2017-12-03 15:43:31 +0900

    comment

--- source/blib/asyncresult.h
+++ source/blib/asyncresult.h
@@ -5,7 +5,7 @@
  * Released under the MIT License
  * https://github.com/bluebackblue/brownie/blob/master/LICENSE
  * http://bbbproject.sakura.ne.jp/wordpress/mitlicense
- * @brief 「shareptr」「weakptr」。
+ * @brief スレッド間での値の共有。
 */
 
 
