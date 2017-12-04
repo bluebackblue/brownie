bc9ef584b85dd6b6b5e1a9dc4975a474310aef75
blueback <blueback28@gmail.com>
2017-12-04 20:54:20 +0900

    SymGetLineFromAddr bugfix

--- source/blib/stacktrace.cpp
+++ source/blib/stacktrace.cpp
@@ -81,7 +81,7 @@ namespace NBlib
 
 				IMAGEHLP_SYMBOL* t_symbol = nullptr;
 				{
-					static u8 s_symbol_data[sizeof(IMAGEHLP_SYMBOL) + 255 * sizeof(char) + sizeof(char)];
+					static u8 s_symbol_data[sizeof(IMAGEHLP_SYMBOL) + 1024 * sizeof(char) + sizeof(char)] = {0};
 					t_symbol = reinterpret_cast< IMAGEHLP_SYMBOL* >(s_symbol_data);
 					t_symbol->MaxNameLength = sizeof(s_symbol_data) - sizeof(IMAGEHLP_SYMBOL) - sizeof(char);
 					t_symbol->SizeOfStruct = sizeof(IMAGEHLP_SYMBOL);
@@ -89,7 +89,7 @@ namespace NBlib
 
 				IMAGEHLP_LINE* t_line = nullptr;
 				{
-					static u8 s_line_data[sizeof(IMAGEHLP_LINE) + 255 * sizeof(char) + sizeof(char)] = {0};
+					static u8 s_line_data[sizeof(IMAGEHLP_LINE) + sizeof(char)] = {0};
 					t_line = reinterpret_cast< IMAGEHLP_LINE* >(s_line_data);
 					t_line->SizeOfStruct = sizeof(IMAGEHLP_LINE);
 				}
@@ -98,11 +98,22 @@ namespace NBlib
 
 				for(s32 ii=0;ii<t_stack_count;ii++){
 					if((a_index < 0)||(ii == a_index)){
-						::SymGetSymFromAddr(t_process,(DWORD64)(t_stack_list[ii]),0,t_symbol);
 
-						::SymGetLineFromAddr(t_process,(DWORD64)(t_stack_list[ii]),0,t_line);
+						#if defined(ROM_32BIT)
+						DWORD t_disp_symbol = 0;
+						DWORD t_disp_line = 0;
+						::SymGetSymFromAddr(t_process,(DWORD)(t_stack_list[ii]),&t_disp_symbol,t_symbol);
+						::SymGetLineFromAddr(t_process,(DWORD)(t_stack_list[ii]),&t_disp_line,t_line);
+						#else
+						DWORD64 t_disp_symbol = 0;
+						DWORD t_disp_line = 0;
+						::SymGetSymFromAddr64(t_process,(DWORD64)(t_stack_list[ii]),&t_disp_symbol,t_symbol);
+						::SymGetLineFromAddr64(t_process,(DWORD64)(t_stack_list[ii]),&t_disp_line,t_line);
+						#endif
 
-						char t_buffer[64] = {0};
+						char t_buffer[256] = {0};
+
+						a_out_string += VASTRING(t_buffer,sizeof(t_buffer),"%s(%d) : ",t_line->FileName,t_line->LineNumber);
 
 						#if defined(ROM_32BIT)
 						a_out_string += VASTRING(t_buffer,sizeof(t_buffer),"0x%08x : ",t_stack_list[ii]);
@@ -111,6 +122,7 @@ namespace NBlib
 						#endif
 
 						a_out_string += t_symbol->Name;
+
 						a_out_string += "\n";
 					}
 				}
