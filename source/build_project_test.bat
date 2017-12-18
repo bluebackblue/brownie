dfe73a6c1e5ccad4efb495eaa212c9d230d2843e
blueback <blueback28@gmail.com>
2017-12-18 19:16:47 +0900

    build

--- source/build_project_test.bat
+++ source/build_project_test.bat
@@ -1,9 +1,20 @@
 CALL ..\..\..\setting.bat
 
+FOR /F %%D IN (TestDefine.txt) DO (
+
+	ECHO #define DEF_TEST_INDEX %%D > .\project_test\brownie_config\test.h
+
+)
+
+
+@PAUSE
+
 @ECHO #define DEF_TEST_INDEX 12 > .\project_test\brownie_config\test.h
 %MSBUILD% brownie.vcxproj /t:rebuild /p:Configuration=Debug;Platform=Win32
 
 @PAUSE
+
+
 
 FOR /F %%C IN (Configuration.txt) DO (
 	FOR /F %%P IN (Platform.txt) DO (
