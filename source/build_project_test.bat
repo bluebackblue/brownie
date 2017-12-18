19139ea2165128774184c37a8045310fecc9be61
blueback <blueback28@gmail.com>
2017-12-18 20:56:56 +0900

    build

--- source/build_project_test.bat
+++ source/build_project_test.bat
@@ -7,11 +7,14 @@ FOR /F %%D IN (TestDefine.txt) DO (
 	FOR /F %%C IN (Configuration.txt) DO (
 		FOR /F %%P IN (Platform.txt) DO (
 
+			@ECHO %%C %%P %%D >> build_log.txt
+
 			@REM ヘッダー作成。
-			@ECHO #define DEF_TEST_INDEX %%D > .\project_test\brownie_config\test.h
+			@ECHO #define DEF_TEST_AUTO (1) > .\project_test\brownie_config\test.h
+			@ECHO #define DEF_TEST_INDEX %%D >> .\project_test\brownie_config\test.h
 
 			@REM ビルド。
-			%MSBUILD% brownie.vcxproj /t:rebuild /p:Configuration=%%C;Platform=%%P >> build_log.txt
+			%MSBUILD% brownie.vcxproj /t:rebuild /p:Configuration=%%C;Platform=%%P
 
 			@REM 移動。
 			move .\bin\%%P\%%C\brownie.exe brownie_%%P_%%C_%%D.exe
@@ -19,11 +22,14 @@ FOR /F %%D IN (TestDefine.txt) DO (
 	)
 )
 
+@ECHO END >> build_log.txt
+
 FOR /F %%D IN (TestDefine.txt) DO (
 	FOR /F %%C IN (Configuration.txt) DO (
 		FOR /F %%P IN (Platform.txt) DO (
 
 			IF EXIST brownie_%%P_%%C_%%D.exe (
+				@ECHO RUN : brownie_%%P_%%C_%%D.exe >> auto_log.txt
 				brownie_%%P_%%C_%%D.exe
 			) ELSE (
 				@ECHO ERROR : brownie_%%P_%%C_%%D.exe >> auto_log.txt
@@ -33,5 +39,6 @@ FOR /F %%D IN (TestDefine.txt) DO (
 	)
 )
 
-@PAUSE
+
+@ECHO END >> auto_log.txt
 
