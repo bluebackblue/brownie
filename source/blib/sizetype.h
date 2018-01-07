a5a1bb69dabf0e0d80bf5156ee953695a4f21d96
blueback <blueback28@gmail.com>
2018-01-08 03:10:03 +0900

    整理

--- source/blib/sizetype.h
+++ source/blib/sizetype.h
@@ -5,7 +5,7 @@
  * Released under the MIT License
  * https://github.com/bluebackblue/brownie/blob/master/LICENSE.txt
  * http://bbbproject.sakura.ne.jp/wordpress/mitlicense
- * @brief 矩形。
+ * @brief サイズ。
 */
 
 
